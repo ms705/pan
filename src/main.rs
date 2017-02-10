eb823f34d5cf3b95fa6f1f0b3a656d1c2cf72ba8
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-02-10 12:52:44 -0500

    Initial prototype

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+extern crate distributary;
+extern crate rustyline;
+extern crate slog;
+extern crate slog_term;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+use slog::DrainExt;
+
+fn main() {
+    // `()` means no completer is required
+    let mut rl = Editor::<()>::new();
+
+    println!("\nWelcome to Pan, your interactive Soup migration shell!\n");
+
+    if let Err(_) = rl.load_history("history.txt") {
+        println!("No previous history.");
+    }
+
+    let mut g = distributary::Blender::new();
+    g.log_with(slog::Logger::root(slog_term::streamer().full().build().fuse(), None));
+
+    let mut current_recipe = distributary::Recipe::blank();
+
+    loop {
+        let readline = rl.readline("Pan> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+
+                let prev_recipe = current_recipe.clone();
+
+                // try to add query to recipe
+                current_recipe = match current_recipe.extend(&line) {
+                    Ok(mut new_recipe) => {
+                        let mut mig = g.start_migration();
+                        match new_recipe.activate(&mut mig) {
+                            Ok(_) => {
+                                mig.commit();
+                                println!("\n");
+                                new_recipe
+                            }
+                            Err(e) => {
+                                println!("{}", e);
+                                prev_recipe
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        println!("{}", e);
+                        prev_recipe
+                    }
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("^C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    rl.save_history("history.txt").unwrap();
+}
