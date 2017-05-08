c509c4db8d92a85fb7eae6b3f649ba75b7315a55
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-05-07 22:46:48 -0400

    Restructure for more features.
     * INSERT queries are now gated into a different (incomplete) control
       flow path that we will use to execute them.
     * Errors are now reported via slog.
     * Migration code factored out into `migration` function.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
 extern crate distributary;
+extern crate nom_sql;
 extern crate rustyline;
+#[macro_use]
 extern crate slog;
 extern crate slog_term;
 
+use distributary::{Blender, Recipe};
+use nom_sql::SqlQuery;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
-
 use slog::{Level, LevelFilter};
 use slog::DrainExt;
+
+fn migrate(current_recipe: Recipe, g: &mut Blender, line: &str) -> Result<Recipe, String> {
+    // try to add query to recipe
+    match current_recipe.extend(line) {
+        Ok(mut new_recipe) => {
+            let mut mig = g.start_migration();
+            match new_recipe.activate(&mut mig, false) {
+                Ok(_) => {
+                    mig.commit();
+                    Ok(new_recipe)
+                }
+                Err(e) => Err(e),
+            }
+        }
+        Err(e) => Err(e),
+    }
+}
 
 fn main() {
     // `()` means no completer is required
     let mut rl = Editor::<()>::new();
 
-    println!("\nWelcome to Pan, your interactive Soup migration shell!\n");
+    println!("\nWelcome to Pan, your interactive Soup shell!\n");
 
     if let Err(_) = rl.load_history(".pan_history") {
         println!("No previous history.");
     }
 
     let mut g = distributary::Blender::new();
-    g.log_with(slog::Logger::root(LevelFilter::new(slog_term::streamer().build().fuse(),
-                                                   Level::Info),
-                                  None));
+    let log = slog::Logger::root(LevelFilter::new(slog_term::streamer().build().fuse(),
+                                                  Level::Info),
+                                 None);
+    g.log_with(log.clone());
 
     let mut current_recipe = distributary::Recipe::blank(None);
 
@@ -32,29 +53,38 @@ fn main() {
             Ok(line) => {
                 rl.add_history_entry(&line);
 
-                let prev_recipe = current_recipe.clone();
+                if line.is_empty() {
+                    continue;
+                }
 
-                // try to add query to recipe
-                current_recipe = match current_recipe.extend(&line) {
-                    Ok(mut new_recipe) => {
-                        let mut mig = g.start_migration();
-                        match new_recipe.activate(&mut mig, false) {
-                            Ok(_) => {
-                                mig.commit();
-                                println!("\n");
-                                new_recipe
+                match nom_sql::parse_query(&line) {
+                    Ok(q) => {
+                        match q {
+                            SqlQuery::Insert(_) => {
+                                // if this is an INSERT query, we want to execute it using an appropriate mutator
+                                error!(log, "INSERT queries unsupported");
                             }
-                            Err(e) => {
-                                println!("{}", e);
-                                prev_recipe
+                            SqlQuery::CreateTable(_) |
+                            SqlQuery::Select(_) => {
+                                let prev_recipe = current_recipe.clone();
+                                current_recipe = match migrate(current_recipe, &mut g, &line) {
+                                    Ok(new_recipe) => {
+                                        println!("\n");
+                                        new_recipe
+                                    }
+                                    Err(e) => {
+                                        error!(log, "{}", e);
+                                        prev_recipe
+                                    }
+                                }
                             }
                         }
                     }
                     Err(e) => {
-                        println!("{}", e);
-                        prev_recipe
+                        error!(log, "{}", e);
                     }
                 }
+
             }
             Err(ReadlineError::Interrupted) => {
                 println!("^C");
